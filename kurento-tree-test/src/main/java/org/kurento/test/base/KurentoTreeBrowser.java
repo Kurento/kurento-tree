f082ddaa44236fe836f0836850238469c9d1136b
Boni Garcia <bgarcia@gsyc.es>
2015-09-16 14:35:15 +0200

    [kurento-tree-test] Added extension of kurento-test infrastructure
    Change-Id: I8b16e4b90c93cdb4c4bdaf4feb9c903ad081ba3f

---
+++ kurento-tree-test/src/main/java/org/kurento/test/base/KurentoTreeBrowser.java
@@ -0,0 +1,116 @@
+/*
+ * (C) Copyright 2015 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+package org.kurento.test.base;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.kurento.jsonrpc.JsonUtils;
+import org.kurento.test.client.KurentoTestClient;
+import org.kurento.test.client.SdpOfferProcessor;
+import org.kurento.test.client.WebRtcChannel;
+import org.kurento.test.client.WebRtcMode;
+import org.kurento.tree.client.KurentoTreeClient;
+import org.kurento.tree.client.TreeEndpoint;
+import org.kurento.tree.client.internal.IceCandidateInfo;
+
+import com.google.gson.JsonObject;
+
+/**
+ * Specific browser page for kurento-tree tests.
+ * 
+ * @author Boni Garcia (bgarcia@gsyc.es)
+ * @since 6.1.1
+ */
+public class KurentoTreeBrowser extends KurentoTestClient {
+
+	public KurentoTreeBrowser() {
+	}
+
+	public KurentoTreeBrowser(KurentoTreeBrowser client) {
+		super(client);
+	}
+
+	public void setTreeSource(KurentoTreeClient kurentoTree, String treeId, WebRtcChannel channel, WebRtcMode mode)
+			throws InterruptedException {
+		internalTreeManagement(kurentoTree, treeId, channel, mode);
+	}
+
+	public void addTreeSink(KurentoTreeClient kurentoTree, String treeId, WebRtcChannel channel, WebRtcMode mode)
+			throws InterruptedException {
+		internalTreeManagement(kurentoTree, treeId, channel, mode);
+	}
+
+	@SuppressWarnings("deprecation")
+	public void internalTreeManagement(final KurentoTreeClient kurentoTree, final String treeId,
+			final WebRtcChannel channel, final WebRtcMode mode) throws InterruptedException {
+
+		Thread notif = new Thread("notif") {
+			public void run() {
+				log.info("Starting gathering candidates from server by polling blocking queue");
+				while (true) {
+					try {
+						IceCandidateInfo candidateInfo = kurentoTree.getServerCandidate();
+
+						if (candidateInfo == null) {
+							log.info("Finished gathering candidates from server (notif thread exiting)");
+							return;
+						}
+
+						JsonObject candidate = JsonUtils.toJsonObject(candidateInfo.getIceCandidate());
+						log.debug("Sending candidate {}", candidate);
+
+						browserClient.executeScript("addIceCandidate('" + candidate + "');");
+					} catch (Exception e) {
+						log.warn("Exception while processing ICE candidate and sending notification", e);
+					}
+				}
+
+			}
+		};
+		notif.start();
+
+		final CountDownLatch latch = new CountDownLatch(1);
+		Thread t = new Thread() {
+			public void run() {
+				initWebRtcSdpProcessor(new SdpOfferProcessor() {
+					@Override
+					public String processSdpOffer(String sdpOffer) {
+						String sdpAnswer = null;
+						try {
+							if (mode == WebRtcMode.SEND_ONLY) {
+								sdpAnswer = kurentoTree.setTreeSource(treeId, sdpOffer);
+							} else if (mode == WebRtcMode.RCV_ONLY) {
+								TreeEndpoint treeEndpoint = kurentoTree.addTreeSink(treeId, sdpOffer);
+								sdpAnswer = treeEndpoint.getSdp();
+							}
+
+						} catch (Exception e) {
+							log.error("Exception processing sdp offer", e);
+						}
+						return sdpAnswer;
+					}
+				}, channel, mode);
+				latch.countDown();
+			}
+		};
+		t.start();
+		if (!latch.await(browserClient.getTimeout(), TimeUnit.SECONDS)) {
+			t.interrupt();
+			t.stop();
+		}
+	}
+
+}
---
+++ kurento-tree-test/src/main/java/org/kurento/test/base/KurentoTreeTest.java
@@ -0,0 +1,68 @@
+/*
+ * (C) Copyright 2015 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+package org.kurento.test.base;
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.kurento.test.config.TestScenario;
+import org.kurento.tree.client.KurentoTreeClient;
+
+/**
+ * Base for kurento-tree tests.
+ * 
+ * @author Boni Garcia (bgarcia@gsyc.es)
+ * @since 6.1.1
+ */
+public class KurentoTreeTest extends BrowserKurentoClientTest {
+
+	final static String KTS_WS_URI_PROP = "kts.ws.uri";
+	final static String KTS_WS_URI_DEFAULT = "ws://localhost:8890/kurento-tree";
+
+	protected static KurentoTreeClient kurentoTreeClient;
+
+	public KurentoTreeTest() {
+		super();
+	}
+
+	public KurentoTreeTest(TestScenario testScenario) {
+		super(testScenario);
+		this.setClient(new KurentoTreeBrowser());
+	}
+
+	@Before
+	public void setupKurentoClient() throws IOException {
+		kurentoTreeClient = new KurentoTreeClient(System.getProperty(KTS_WS_URI_PROP, KTS_WS_URI_DEFAULT));
+	}
+
+	@After
+	public void teardownKurentoClient() throws Exception {
+		if (kurentoTreeClient != null) {
+			kurentoTreeClient.close();
+		}
+	}
+
+	@Override
+	public KurentoTreeBrowser getBrowser() {
+		return (KurentoTreeBrowser) super.getBrowser();
+	}
+
+	@Override
+	public KurentoTreeBrowser getBrowser(int index) {
+		return (KurentoTreeBrowser) super.getBrowser(index);
+	}
+
+}
