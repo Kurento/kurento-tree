b20272c0a9657f6fa2b5eb2bbf076cd7b0fae7be
Ivan Gracia <izanmail@gmail.com>
2016-01-15 12:28:25 +0100

    Removed plumber endpoint references
    Change-Id: I0026287f8f5c97c29ce4b7a2610fe4d9e67ab4e2

--- kurento-tree-server/src/main/java/org/kurento/tree/server/kms/real/RealPlumber.java
+++ kurento-tree-server/src/main/java/org/kurento/tree/server/kms/real/RealPlumber.java
@@ -1,9 +1,8 @@
+
 package org.kurento.tree.server.kms.real;
 
 import org.kurento.client.MediaElement;
 import org.kurento.client.WebRtcEndpoint;
-import org.kurento.commons.PropertiesManager;
-import org.kurento.module.plumberendpoint.PlumberEndpoint;
 import org.kurento.tree.server.kms.Element;
 import org.kurento.tree.server.kms.Plumber;
 import org.slf4j.Logger;
@@ -11,20 +10,13 @@ import org.slf4j.LoggerFactory;
 
 public class RealPlumber extends Plumber implements RealElement {
 
-  private static final boolean USE_WEBRTC_AS_PLUMBER = PropertiesManager
-      .getProperty("useWebRtcAsPlumber", true);
-
   private static Logger log = LoggerFactory.getLogger(RealPlumber.class);
 
   private MediaElement endpoint;
 
   public RealPlumber(RealPipeline pipeline) {
     super(pipeline);
-    if (USE_WEBRTC_AS_PLUMBER) {
-      endpoint = new WebRtcEndpoint.Builder(pipeline.getMediaPipeline()).build();
-    } else {
-      endpoint = new PlumberEndpoint.Builder(pipeline.getMediaPipeline()).build();
-    }
+    endpoint = new WebRtcEndpoint.Builder(pipeline.getMediaPipeline()).build();
 
     if (getLabel() != null) {
       endpoint.setName(getLabel());
@@ -53,32 +45,20 @@ public class RealPlumber extends Plumber implements RealElement {
 
     super.link(plumber);
 
-    if (USE_WEBRTC_AS_PLUMBER) {
+    WebRtcEndpoint thisWebRtc = (WebRtcEndpoint) endpoint;
+    WebRtcEndpoint otherWebRtc = (WebRtcEndpoint) ((RealPlumber) plumber).endpoint;
 
-      WebRtcEndpoint thisWebRtc = (WebRtcEndpoint) endpoint;
-      WebRtcEndpoint otherWebRtc = (WebRtcEndpoint) ((RealPlumber) plumber).endpoint;
+    log.debug("Connecting webRtcs from Kms {} to Kms {}", this.getPipeline().getKms(),
+        plumber.getPipeline().getKms());
 
-      log.debug("Connecting webRtcs from Kms {} to Kms {}", this.getPipeline().getKms(),
-          plumber.getPipeline().getKms());
+    thisWebRtc.addOnIceCandidateListener((e) -> otherWebRtc.addIceCandidate(e.getCandidate()));
+    otherWebRtc.addOnIceCandidateListener((e) -> thisWebRtc.addIceCandidate(e.getCandidate()));
 
-      thisWebRtc.addOnIceCandidateListener((e) -> otherWebRtc.addIceCandidate(e.getCandidate()));
-      otherWebRtc.addOnIceCandidateListener((e) -> thisWebRtc.addIceCandidate(e.getCandidate()));
+    thisWebRtc.processAnswer(otherWebRtc.processOffer(thisWebRtc.generateOffer()));
 
-      thisWebRtc.processAnswer(otherWebRtc.processOffer(thisWebRtc.generateOffer()));
+    thisWebRtc.gatherCandidates();
+    otherWebRtc.gatherCandidates();
 
-      thisWebRtc.gatherCandidates();
-      otherWebRtc.gatherCandidates();
-
-    } else {
-
-      PlumberEndpoint thisPlumber = (PlumberEndpoint) endpoint;
-      PlumberEndpoint otherPlumber = (PlumberEndpoint) ((RealPlumber) plumber).endpoint;
-
-      String address = otherPlumber.getAddress();
-      int port = otherPlumber.getPort();
-      log.debug("Connecting plumber to adress:" + address + " port:" + port);
-      thisPlumber.link(address, port);
-    }
   }
 
   @Override
